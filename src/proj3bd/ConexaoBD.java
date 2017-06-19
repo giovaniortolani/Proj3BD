076e153eade2591b34322aae94a8eecd6be1406a
Bernardo Duarte <bernardolageduarte@gmail.com>
2017-06-19 11:37:16 -0300

    conexao BD

---
+++ src/proj3bd/ConexaoBD.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj3bd;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author bernardold
+ */
+public class ConexaoBD {
+
+    private static ConexaoBD instance;
+
+    public static Connection conn; 
+    public static Statement stmt = null;
+
+    private ConexaoBD() throws SQLException { 
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        }
+        
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:oracle:thin:@grad.icmc.usp.br:15215:orcl",
+                    "8598861",
+                    "bernardo");
+
+            stmt = conn.createStatement();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public static ConexaoBD getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new ConexaoBD();
+        }
+        return instance; 
+    }
+    
+    public void destroy () throws SQLException {
+        conn.close();
+        stmt.close();
+        instance = null;
+    }
+}
